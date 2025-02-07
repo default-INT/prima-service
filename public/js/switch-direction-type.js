c0926efd4f0ca83cca5465d8dd0b15e741b078a3
Yauheni Trafimau <yauheni.trafimau@itechart-group.com>
2025-02-07 14:48:23 +0300

    add animation for switch

--- public/js/switch-direction-type.js
+++ public/js/switch-direction-type.js
@@ -7,6 +7,7 @@ const importBox = document.getElementById('importBox');
 const exportBox = document.getElementById('exportBox');
 
 const selectingButtons = document.querySelectorAll('#selectDirectionType > button');
+const selectDirectionType = document.getElementById('selectDirectionType')
 
 const buttonByType = {
   [directionType.import]: selectingButtons[0],
@@ -21,7 +22,13 @@ const boxByType = {
 selectingButtons.forEach(button => {
   button.addEventListener('click', (event) => {
     const selectedType = event.target.dataset.type
+    const direction = event.target.dataset.direction
     const reversedType = selectedType === directionType.import ? directionType.export : directionType.import
+
+    if (buttonByType[selectedType].classList.contains('active')) return
+
+    selectDirectionType.classList.remove('left', 'right');
+    selectDirectionType.classList.add(direction);
 
     buttonByType[selectedType].classList.add('active')
     buttonByType[reversedType].classList.remove('active')
